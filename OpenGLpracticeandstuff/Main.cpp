24f50064c0da05165392b5d0f383c7c4ab76d2ba
shan <codeitchy@gmail.com>
2024-10-02 22:42:29 +0530

    Added Textures

--- OpenGLpracticeandstuff/Main.cpp
+++ OpenGLpracticeandstuff/Main.cpp
@@ -1,8 +1,8 @@
-
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include "stb_image.h"
 
-#include "Shader.h"
+#include "shader.h"
 
 #include <iostream>
 
@@ -21,7 +21,7 @@ int main()
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);		// If on Mac OS X
 
-	GLFWwindow* window = glfwCreateWindow(800, 600, "Window Created, Yatta!!", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(800, 800, "Window Created, Yatta!!", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GFL Window" << std::endl;
@@ -42,49 +42,101 @@ int main()
 	// Vertex Input
 	
 	float vertices[] = {
-		// To render 2D triangle
-		// Positions		// Colours
-		0.5f, -0.5f, 0.0f,	1.0f, 0.0f, 0.0f,		// Bottom right
-		-0.5f, -0.5f, 0.0f,	0.0f, 1.0f, 0.0f,		// Bottom left
-		0.0f, 0.5f, 0.0f,	0.0f, 0.0f, 1.0f		// Top
+		// positions          // colors           // texture coords
+		 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+		 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
 	};
 
-	float texCoords[] =
-	{
-		0.0f, 0.0f,									// Bottom Left	
-		1.0f, 0.0f,									// Bottom Right
-		0.5f, 1.0f									// Top
+	unsigned int indices[] = {
+		0, 1, 3, // first triangle
+		1, 2, 3  // second triangle
 	};
 
-	// To create a Rectangle
-	//unsigned int indices[] = {
-	//	0, 1, 3,	// First Triangle
-	//	1, 2, 3		// Second Triangle
-	//};
-
-
-	unsigned int VBO, VAO;
+	unsigned int VBO, VAO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 	// position attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 	// color attribute
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
+	// texture attribute
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
 
 	// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
 	// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
 	// glBindVertexArray(0);
 
 
+	// Loading Textures
+	unsigned int texture1, texture2;
 	
+	// Texture Reimu
+	glGenTextures(1, &texture1);
+	glBindTexture(GL_TEXTURE_2D, texture1);
+	// Texture Wrapping
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	// Texture Filtering
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	// Image Loading, Texture Creation, Mipmap Generation
+	stbi_set_flip_vertically_on_load(true);
+	int width, height, nrChannels;
+	unsigned char* data = stbi_load("data/reimu.jpg", &width, &height, &nrChannels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_image_free(data);
+
+	// Texture Wall
+	glGenTextures(1, &texture2);
+	glBindTexture(GL_TEXTURE_2D, texture2);
+	// Texture Wrapping
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	// Texture Filtering
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	// Image Loading, Texture Creation, Mipmap Generation
+	stbi_set_flip_vertically_on_load(true);
+	data = stbi_load("data/stone01.jpg", &width, &height, &nrChannels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_image_free(data);
+
+	// Setting uniforms
+	ourShader.use();
+	glUniform1i(glGetUniformLocation(ourShader.ID, "texture2"), 0);
+	ourShader.setInt("texture2", 1);
+
 
 	// Render Loop (Frame)
 	while (!glfwWindowShouldClose(window))
@@ -93,13 +145,19 @@ int main()
 		processInput(window);
 
 		// Rendering
-		glClearColor(0.2f, 0.3f, 0.6f, 1.0f);
+		glClearColor(0.6f, 0.0f, 0.2f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Bind Texture
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture1);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, texture2);
 
 		// Render the triangle
 		ourShader.use();
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT,0);
 
 		// Swap buffers, Check and call event
 		glfwSwapBuffers(window);
@@ -109,7 +167,7 @@ int main()
 	// De-allocate all resources
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
-	//glDeleteBuffers(1, &EBO);
+	glDeleteBuffers(1, &EBO);
 
 	// Terminate all GLFW
 	glfwTerminate();
--- OpenGLpracticeandstuff/shader.fs
+++ OpenGLpracticeandstuff/shader.fs
@@ -2,8 +2,14 @@
 out vec4 FragColor;
 
 in vec3 ourColor;
+in vec2 TexCoord;
+
+// texture sampler
+uniform sampler2D texture1;
+uniform sampler2D texture2;
 
 void main()
 {
-    FragColor = vec4(ourColor, 1.0f);
+    //FragColor = texture(texture1, TexCoord);
+    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.6);
 }
