95ed2f4973b67d1d7ad122778132ae4d5b64de2c
shan <codeitchy@gmail.com>
2024-10-14 15:01:56 +0530

    Added camera

--- OpenGLpracticeandstuff/Main.cpp
+++ OpenGLpracticeandstuff/Main.cpp
@@ -7,15 +7,29 @@
 #include <glm/gtc/type_ptr.hpp>
 
 #include "shader.h"
+#include "camera.h"
 
 #include <iostream>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-
+void mouse_call_back(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void cursor_enter_callback(GLFWwindow* window, int entry);
 void processInput(GLFWwindow* window);
 
+// Settings
 const unsigned int fullWidth = 800;
-const unsigned int fullHeight = 800;
+const unsigned int fullHeight = 600;
+
+// Camera Settings
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+float lastX = fullWidth / 2.0f;
+float lastY = fullHeight / 2.0f;
+bool firstMouse = true;
+
+// Timing
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
 
 float mixValue = 0.0f;
 
@@ -39,6 +53,11 @@ int main()
 	}
 	glfwMakeContextCurrent(window);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetCursorPosCallback(window, mouse_call_back);
+	glfwSetScrollCallback(window, scroll_callback);
+	glfwSetCursorEnterCallback(window, cursor_enter_callback);
+
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
@@ -109,19 +128,6 @@ int main()
 		glm::vec3(-1.3f,  1.0f, -1.5f)
 	};
 
-	//float vertices[] = {
-	//	// positions          // colors           // texture coords
-	//	 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
-	//	 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
-	//	-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
-	//	-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
-	//};
-
-	//unsigned int indices[] = {
-	//	0, 1, 3, // first triangle
-	//	1, 2, 3  // second triangle
-	//};
-
 	unsigned int VBO, VAO; //EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
@@ -145,7 +151,6 @@ int main()
 	// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
 	// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
 	// glBindVertexArray(0);
-
 
 	// Loading Textures
 	unsigned int texture1, texture2;
@@ -202,15 +207,19 @@ int main()
 	ourShader.setInt("texture1", 0);
 	ourShader.setInt("texture2", 1);
 
-
 	// Render Loop (Frame)
 	while (!glfwWindowShouldClose(window))
 	{
+		// Per frame time
+		float currentFrame = static_cast<float>(glfwGetTime());
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
 		// Input
 		processInput(window);
 
 		// Rendering
-		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClearColor(0.6f, 0.2f, 0.4f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		// Bind Texture
@@ -219,31 +228,22 @@ int main()
 		glActiveTexture(GL_TEXTURE1);
 		glBindTexture(GL_TEXTURE_2D, texture2);
 
-		// Creating model, view and projection matrices
-		//glm::mat4 model = glm::mat4(1.0f);
-		glm::mat4 view = glm::mat4(1.0f);
+		ourShader.use();
+		
 		glm::mat4 projection = glm::mat4(1.0f);
+		glm::mat4 view = glm::mat4(1.0f);
+		projection = glm::perspective(glm::radians(camera.Zoom), (float)fullWidth / (float)fullHeight, 0.1f, 100.0f);
+		view = camera.GetViewMatrix();
 
-		//model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
-		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-		projection = glm::perspective(glm::radians(45.0f), (float)fullWidth / (float)fullHeight, 0.1f, 100.0f);
+		ourShader.setMat4("projection", projection);
+		ourShader.setMat4("view", view);
 
 		// Set texture mix value in the shader
-		ourShader.use();
 		ourShader.setFloat("mixValue", mixValue);
-
-		// Get matrix uniform location, set matrix
-		//int modelLoc = glGetUniformLocation(ourShader.ID, "model");
-		int viewLoc = glGetUniformLocation(ourShader.ID, "view");
-		int projectionLoc = glGetUniformLocation(ourShader.ID, "projection");
-		//glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-		glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
 		// Render the rectangle
 		glBindVertexArray(VAO);
-		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT,0);
-		//glDrawArrays(GL_TRIANGLES, 0, 36);
+
 		for (unsigned int i = 0; i < 10; i++)
 		{
 			glm::mat4 model = glm::mat4(1.0f);
@@ -277,6 +277,7 @@ void processInput(GLFWwindow* window)
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
 
+	// Adjust texture mix value
 	if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
 	{
 		mixValue += 0.001f; 
@@ -290,11 +291,55 @@ void processInput(GLFWwindow* window)
 			mixValue = 0.0f;
 	}
 
+	// Camera Inputs
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		camera.ProcessKeyboard(FORWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera.ProcessKeyboard(BACKWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		camera.ProcessKeyboard(LEFT, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		camera.ProcessKeyboard(RIGHT, deltaTime);
+
 	// Enable/Disable Wireframe Mode
 	if (glfwGetKey(window, GLFW_KEY_TAB) == GLFW_PRESS)
 		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 	if (glfwGetKey(window, GLFW_KEY_TAB) == GLFW_RELEASE)
 		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+}
+
+void mouse_call_back(GLFWwindow* window, double xposIn, double yposIn)
+{
+	float xpos = static_cast<float>(xposIn);
+	float ypos = static_cast<float>(yposIn);
+
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+	lastX = xpos;
+	lastY = ypos;
+
+	camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	camera.ProcessMouseScroll(static_cast<float>(yoffset));
+}
+
+void cursor_enter_callback(GLFWwindow* window, int entry)
+{
+	if (entry)
+	{
+		firstMouse = true;
+	}
 }
 
 // If User or OS decides to resize window
